c3b06c9d3d8e0b4837cb4a7e84e5aa4e56d6adb4
Hammania689 <hameedabdulrashid@gmail.com>
2019-07-14 14:18:50 -0400

    Typo corrections

--- pulse/pulse_settings.sh
+++ pulse/pulse_settings.sh
@@ -1,7 +1,7 @@
 
 # Change pulse default settings
 # Removing suspend on idle
-sudo mv default.pa /etc/pulse/default.pa 
+sudo cp default.pa /etc/pulse/default.pa 
 
 # Add changeSinks to path and change default sink to sink 1
 sudo cp changeSinks /usr/bin/changeSinks
