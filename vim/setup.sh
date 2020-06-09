e7fdb2b3aabc54847a8b4dcaded75ef280d55304
Hammania689 <hameedabdulrashid@gmail.com>
2020-06-10 08:21:21 +0900

    Vim setup script

---
+++ vim/setup.sh
@@ -0,0 +1,9 @@
+# Install vim with +clipboard support: http://vimcasts.org/blog/2013/11/getting-vim-with-clipboard-support/
+sudo apt install vim-gnome
+
+# Install vundle plugin manager
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# Copy vim config from the cwd to the ~
+cp ./.vimrc ~/.vimrc
+
