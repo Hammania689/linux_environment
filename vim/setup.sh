75a7bbd39060183899ccaf9bd94f722e1da995d8
Hammania689 <hameedabdulrashid@gmail.com>
2020-06-10 08:33:50 +0900

    Powerline font install command and restart shell

--- vim/setup.sh
+++ vim/setup.sh
@@ -1,5 +1,8 @@
+# Install powerline font
+./install_powerline.sh
+
 # Install vim with +clipboard support: http://vimcasts.org/blog/2013/11/getting-vim-with-clipboard-support/
-sudo apt install vim-gnome
+sudo apt install vim vim-gnome
 
 # Install vundle plugin manager
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
@@ -7,3 +10,5 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 # Copy vim config from the cwd to the ~
 cp ./.vimrc ~/.vimrc
 
+# Restart shell
+exec $SHELL
