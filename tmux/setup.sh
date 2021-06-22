26cd6491c54ef4b16e5891fb956305c7b07c0514
Hammania689 <hameedabdulrashid@gmail.com>
2021-06-22 03:04:36 -0500

    Basic script to get tmux + plugins setup and going

---
+++ tmux/setup.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+
+mv .tmux.conf ~/
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+
+echo "You are all set with tmux. Run the following cmds to install plugins and reload the session:"
+echo "{tmux prefix} + I"
+echo "tmux source ~/.tmux.conf"
