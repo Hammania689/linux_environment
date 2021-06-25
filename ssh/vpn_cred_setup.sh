5588ac606dbb4d0933085a5b1094713bcad53415
Hammania689 <hameedabdulrashid@gmail.com>
2021-06-25 02:30:05 -0500

    Instructions on how to setup vpn cred file

--- ssh/vpn_cred_setup.sh
+++ ssh/vpn_cred_setup.sh
@@ -1,5 +1,4 @@
 #!/usr/bin
-
 echo "Create a file name ./vpn_creds and place the following inside:"
 printf """0
 username
@@ -7,3 +6,4 @@ password
 y
 exit
 """
+echo "Refer here: https://superuser.com/a/726542"
--- util_scripts/non_cudapackages.sh
+++ util_scripts/non_cudapackages.sh
@@ -27,38 +27,38 @@ sudo groupadd docker
 sudo usermod -aG docker $USER
 #######################################################################
 # Pyenv
-echo "Installing Pyenv"
-git clone https://github.com/pyenv/pyenv.git ~/.pyenv
-echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
-echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
-echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.bashrc
-exec "$SHELL"
+# echo "Installing Pyenv"
+# git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+# echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+# echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+# echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.bashrc
+# exec "$SHELL"
 
 # Pyenv-virtualenv
-echo "Installing Pyenv-virtualenv"
-git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
-printf '\n' >> ~/.bashrc
-printf '# Pyenv Virtualenv AutoCompletion'  >> ~/.bashrc
-echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
-exec "$SHELL"
-
+# echo "Installing Pyenv-virtualenv"
+# git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
+# printf '\n' >> ~/.bashrc
+# printf '# Pyenv Virtualenv AutoCompletion'  >> ~/.bashrc
+# echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
+# exec "$SHELL"
+# 
+# ########################################################################
+# # VS Code
+# curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+# sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+# sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+# 
+# sudo apt-get install -y apt-transport-https
+# sudo apt-get update
+# sudo apt-get install -y code # or code-insiders
+# 
+# # Set Code as default text editor
+# sudo update-alternatives --set editor /usr/bin/code
 ########################################################################
-# VS Code
-curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
-sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-
-sudo apt-get install -y apt-transport-https
-sudo apt-get update
-sudo apt-get install -y code # or code-insiders
-
-# Set Code as default text editor
-sudo update-alternatives --set editor /usr/bin/code
-########################################################################
 
 sudo apt-get update
 
-INSTALL_PKGS="tmux python3-tk openssh-server"
+INSTALL_PKGS="tmux python3-tk openssh-server gnome-shell-pomodoro ranger"
 for i in $INSTALL_PKGS; do
   sudo apt-get install -y $i
 done
@@ -71,3 +71,5 @@ touch jetbrainsLoginInfo.txt
 printf "Hammania689\ns8e5tknH0rVo" >> jetbrainsLoginInfo.txt
 
 sudo snap install spotify 
+
+
